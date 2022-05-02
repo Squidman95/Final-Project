eb3fdc44e7f27bcf83d8b7c456e52bbb0de6b275
Mikkel Stender <mikkel-s.mikkelstender@gmail.com>
2022-05-02 20:57:46 +0200

    topbar

--- src/Components/ButtonDropdown/ButtonDropdown.jsx
+++ src/Components/ButtonDropdown/ButtonDropdown.jsx
@@ -3,7 +3,14 @@ import "./ButtonDropdown.scss";
 
 const ButtonDropdown = () => {
     return (
-        <button>Hello</button>
+        <div class="dropdown">
+            <button class="dropbtn">Dropdown</button>
+            <div class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
     )
 }
 
--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Topbar.scss";
 import ButtonDropdown from "../ButtonDropdown/ButtonDropdown";
 
-const Topbar = (props) => {
+/*const Topbar = (props) => {
   return (
     <div className="Topbar">
       <div className="Topbar-left-container">
@@ -18,6 +18,24 @@ const Topbar = (props) => {
           <ButtonDropdown />
           
           <a href="#BasketPage"><img className="Topbar-right-basketimage" src={require('./basket-icon.png')}></img></a>
+      </div>
+    </div>
+  );
+};
+*/
+const Topbar = (props) => {
+  return (
+    <div class="topnav Topbar">
+      <div className="Topbar-left-container">
+        <a class="active" href="#home">Home</a>
+      </div>
+      <div className="Topbar-middle-container">
+        <a href="#news">News</a>
+      </div>
+      <div className="Topbar-right-container">
+        <a href="#about">Log in</a>
+        <ButtonDropdown/>
+        <a href="#about"><img className="Topbar-right-basketimage" src={require('./basket-icon.png')}></img></a>
       </div>
     </div>
   );
