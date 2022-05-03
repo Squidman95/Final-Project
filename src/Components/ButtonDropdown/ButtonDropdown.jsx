60cc50c1aa37465c12a8f904e0df9119ed8460e1
Mikkel Stender <mikkel-s.mikkelstender@gmail.com>
2022-05-03 21:54:57 +0200

    topbar

--- src/Components/ButtonDropdown/ButtonDropdown.jsx
+++ src/Components/ButtonDropdown/ButtonDropdown.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import "./ButtonDropdown.scss";
 
-const ButtonDropdown = () => {
-    return (
-        <div class="dropdown">
-            <button class="dropbtn">Dropdown</button>
-            <div class="dropdown-content">
-                <a href="#">Link 1</a>
-                <a href="#">Link 2</a>
-                <a href="#">Link 3</a>
-            </div>
-        </div>
-    )
-}
+const ButtonDropdown = (props) => {
+  return (
+    <div class="dropdown">
+      <button class="dropbtn">Dropdown</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+  );
+};
 
-export default ButtonDropdown
+export default ButtonDropdown;
--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -2,40 +2,51 @@ import React from "react";
 import "./Topbar.scss";
 import ButtonDropdown from "../ButtonDropdown/ButtonDropdown";
 
-/*const Topbar = (props) => {
-  return (
-    <div className="Topbar">
-      <div className="Topbar-left-container">
-          <a href="#FrontPage">Home</a>
-      </div>
-      <div className="Topbar-middle-container">
-          <p>Hello us</p>
-
-      </div>
-      <div className="Topbar-right-container">
-          <a>Log out</a>
-          
-          <ButtonDropdown />
-          
-          <a href="#BasketPage"><img className="Topbar-right-basketimage" src={require('./basket-icon.png')}></img></a>
-      </div>
-    </div>
-  );
-};
-*/
 const Topbar = (props) => {
+  const isLoggedIn = props.isLoggedin;
+  if (isLoggedIn) {
+    return (
+      <div class="topnav Topbar">
+        <div className="Topbar-left-container">
+          <a class="active" href="#home">
+            Home
+          </a>
+        </div>
+        <div className="Topbar-middle-container">
+          <a href="#news">News</a>
+        </div>
+        <div className="Topbar-right-container">
+          <a href="#about">Log out</a>
+          <ButtonDropdown />
+          <a href="#about">
+            <img
+              className="Topbar-right-basketimage"
+              src={require("./basket-icon.png")}
+            ></img>
+          </a>
+        </div>
+      </div>
+    );
+  }
   return (
     <div class="topnav Topbar">
       <div className="Topbar-left-container">
-        <a class="active" href="#home">Home</a>
+        <a class="active" href="#home">
+          Home
+        </a>
       </div>
       <div className="Topbar-middle-container">
         <a href="#news">News</a>
       </div>
       <div className="Topbar-right-container">
         <a href="#about">Log in</a>
-        <ButtonDropdown/>
-        <a href="#about"><img className="Topbar-right-basketimage" src={require('./basket-icon.png')}></img></a>
+        <ButtonDropdown />
+        <a href="#about">
+          <img
+            className="Topbar-right-basketimage"
+            src={require("./basket-icon.png")}
+          ></img>
+        </a>
       </div>
     </div>
   );
