27b5676c10c648eff1a2e0fc9fb2fe5284b46e10
Squidman95 <squidmanse@gmail.com>
2022-05-19 10:38:15 +0200

    Frontpage renders SearchResult

--- src/Pages/Frontpage/Frontpage.jsx
+++ src/Pages/Frontpage/Frontpage.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+// import React from "react";
+import React, { useEffect, useState } from 'react';
 import Topbar from "../../Components/Topbar/Topbar";
 import { getAllProducts } from '../../Service/ProductService';
-    
+import Popup from '../../Components/Popup/Popup';    
+import SearchResult from '../../Pages/SearchResultPage/SearchResultPage';    
+
+
 const Frontpage = () => {
+  const [visibility, setVisibility] = useState(false); // For the login/signup popup
 
   getAllProducts().then(function(products) {
     console.log(products);
@@ -11,7 +16,9 @@ const Frontpage = () => {
   return (
     <div>
       <h1>Frontpage</h1>
+      <SearchResult/>
       <Topbar isLoggedIn={true} />
+      {/* <Popup onClose={setVisibility} show={visibility}/>  */}
     </div>
   );
 };
