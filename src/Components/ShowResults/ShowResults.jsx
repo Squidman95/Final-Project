21b43099b613ffb8250845cdac00a45174c0f88a
Squidman95 <squidmanse@gmail.com>
2022-05-20 15:39:24 +0200

    Updated carousel to work with cards

--- src/Components/ShowResults/ShowResults.jsx
+++ src/Components/ShowResults/ShowResults.jsx
@@ -1,54 +1,49 @@
-import React from 'react';
+import React from "react";
 // import products from '../../Data/ProductData';
-import Card from '../Card/Card';
-import './ShowResults.scss';
-import CarouselComponent from '../Carousel/Carousel';
+import Card from "../Card/Card";
+import "./ShowResults.scss";
+import CarouselComponent from "../Carousel/Carousel";
 
 const SearchResult = (props) => {
-    
-    let {
-        products = []
-    } = props;
+  let { products = [] } = props;
 
-    const getCarouselArray = () => {
-        if(products.length > 2){
-            return [products[0].image, products[1].image, products[2].image];
-        } 
-        return [];
+  const getCarouselArray = () => {
+    if (products.length > 2) {
+      return [products[0].image, products[1].image, products[2].image];
     }
+    return [];
+  };
 
-    return (
-
-        <div className='ShowResults'>
-            <div className='ShowResults-Carousel'>
-                <CarouselComponent
-                    imageArray = {getCarouselArray()}
-                    showIndicators = {false}
-                    showArrows = {true}
-                    autoPlay = {true}
-                    interval = {2000}
-                    showThumbs = {false}
-                    infiniteLoop = {true}
-                />
-            </div>
-            <div className='ShowResults-Cards'>
-                {
-                    products.map((item, index) => {
-                        return(
-                            <Card key = {index}
-                                id = {item.id}
-                                image = {item.image}
-                                header = {item.name}
-                                subtext = {item.shortDescription}
-                                price = {item.price}
-                            />
-                        );
-                    })
-                }
-            </div>
-        </div>
-    )
-
-}
+  return (
+    <div className="ShowResults">
+      <div className="ShowResults-Carousel">
+        <CarouselComponent
+          // imageArray = {getCarouselArray()}
+          imageArray={products}
+          showIndicators={false}
+          showArrows={true}
+          autoPlay={true}
+          interval={2000}
+          showThumbs={false}
+          infiniteLoop={true}
+        />
+      </div>
+      <div className="ShowResults-Cards">
+        {products.map((item, index) => {
+          return (
+            <Card
+              key={index}
+              id={item.id}
+              image={item.image}
+              header={item.name}
+              subtext={item.shortDescription}
+              price={item.price}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default SearchResult;
