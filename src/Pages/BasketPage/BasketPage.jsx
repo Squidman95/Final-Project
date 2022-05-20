99a82313cc7fde74e50f727156ed9a765d1f4b14
cema <cema@itu.dk>
2022-05-20 12:35:55 +0200

    Edit css/style for BasketPage

--- src/Pages/BasketPage/BasketPage.jsx
+++ src/Pages/BasketPage/BasketPage.jsx
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import './BasketPage.scss';
 import Button from '../../Components/Button/Button';
-// import products from '../../Data/ProductData';
+//import products from '../../Data/ProductData';
 import Card from '../../Components/Card/Card';
 import { getBasket, createBasket, removeItemFromBasket } from '../../Service/BasketServices';
 
@@ -40,6 +40,9 @@ const BasketPage = (props) => {
 
     return (
         <div className='BasketPage'>
+            <h1 className = 'basketHeader'>
+                Products in basket:
+            </h1>
             <div className='Basket-Cards'>
                 {
                     basket.map((item, index) => {
@@ -58,7 +61,7 @@ const BasketPage = (props) => {
             </div>
 
             <div className='TotalAndButton'>
-                <div id="total">Total: {total} DKK</div>
+                <h3 className="total">Total: {total} DKK</h3>
                 <div className='Checkout-Button'>
                     <Button 
                         to='/SearchResult'
@@ -77,4 +80,3 @@ const BasketPage = (props) => {
 }
 
 export default BasketPage;
-
