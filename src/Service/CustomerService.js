551e90cd1ffd9427e8087f384a88ea2809e485fb
Squidman95 <squidmanse@gmail.com>
2022-05-23 11:32:02 +0200

    Update CustomerService.js

--- src/Service/CustomerService.js
+++ src/Service/CustomerService.js
@@ -1,22 +1,10 @@
-async function createNewCustomer() {
-    var requestOptions = {
-        method: 'POST',
-        redirect: 'follow'
-    };
-      
-    fetch("http://localhost:4000/customers/", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-}
-
-async function createUser(userID, userName, mail, password) {
+async function createCustomer(userID, fname, lname, mail, password) {
     var requestOptions = {
         method: 'POST',
         redirect: 'follow'
     };
     
-    fetch(`http://localhost:4000/customers/${userID}/${userName}/${mail}/${password}`, requestOptions)
+    fetch(`http://localhost:4000/customers/${userID}/${fname}/${lname}/${mail}/${password}`, requestOptions)
     .then(response => response.text())
     .then(result => console.log(result))
     .catch(error => console.log('error', error));
@@ -26,5 +14,15 @@ async function createUser(userID, userName, mail, password) {
 
 
 async function deleteCustomer(userID) {
-
+    var requestOptions = {
+        method: 'DELETE',
+        redirect: 'follow'
+    };
+      
+    fetch(`http://localhost:4000/customers/${userID}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 }
+
+export{createCustomer, deleteCustomer}
