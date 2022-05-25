d6cc4ff811174182cd2d67be30b6bd7d996db602
cema <cema@itu.dk>
2022-05-25 11:01:12 +0200

    Show carousel only when no filter applied + show 'search result' text only when filter applied

--- src/Pages/Frontpage/Frontpage.jsx
+++ src/Pages/Frontpage/Frontpage.jsx
@@ -121,10 +121,21 @@ const Frontpage = (props) => {
       <div className='Frontpage-OfferCarouselContainer'>
         
       </div>
-      <div className="Frontpage-resultscontainer"> {/* //split them into two divs, without them disapearing */}
-      <OfferCarousel
-          products={filteredProducts}
-        />
+      <div className="Frontpage-resultscontainer">
+      <div
+          style={{
+            display: products.length === filteredProducts.length ? "block" : "none",
+            // show the carousel only when no filter is applied
+          }} 
+      >
+          <OfferCarousel
+              products={filteredProducts}
+          />
+      </div>
+      <h2 style={{
+            display: products.length === filteredProducts.length ? "none" : "block",
+            // show 'search result' headline only when a filter has been applied
+          }} >Search result:</h2>
         <ShowResults
           products={filteredProducts}
         />
