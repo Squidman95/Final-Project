151124489cf95e49f6c15b36820361b061ba369e
Squidman95 <squidmanse@gmail.com>
2022-05-25 12:03:06 +0200

    Updated login/signup

--- src/Service/CustomerService.js
+++ src/Service/CustomerService.js
@@ -1,39 +1,45 @@
-async function createCustomer(userID, fname, lname, mail, password) {
-    var requestOptions = {
-        method: 'POST',
-        redirect: 'follow'
+async function createCustomer(userID, fname, lname, email, password) {
+    let userData = {
+        id: userID,
+        fname: fname,
+        lname: lname,
+        email: email,
+        password: password
     };
-    
-    fetch(`http://localhost:4000/customers/${userID}/${fname}/${lname}/${mail}/${password}`, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+
+    fetch("http://localhost:4000/customers/signup",
+        {
+            method: "POST", 
+            body: JSON.stringify(userData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));;
 }
 
-async function login(fname, lname, mail, password) {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
+async function login(fname, lname, email, password) {
+    let userData = {
+        fname: fname,
+        lname: lname,
+        email: email,
+        password: password
     };
     
-    let response = await fetch(`http://localhost:4000/customers/${fname}/${lname}/${mail}/${password}`, requestOptions)
+    let response = await fetch("http://localhost:4000/customers/login",
+                    {
+                        method: "POST", 
+                        body: JSON.stringify(userData),
+                        headers: {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+                        }
+                    });
 
     return await response.json();
 }
-
-// async function login(fname, lname, mail, password) {
-//     var requestOptions = {
-//         method: 'GET',
-//         redirect: 'follow'
-//     };
-      
-//     let response = await fetch(`http://localhost:4000/customers/${fname}/${lname}/${mail}/${password}`, requestOptions)
-//     .then(response => response.text())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
-
-//     return await response.json();
-// }
 
 async function deleteCustomer(userID) {
     var requestOptions = {
